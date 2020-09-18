51c019666efa92e20c9254eb4f27155fc2b91a03
Will Brooks <willrbrooks@cmail.carleton.ca>
2020-09-18 17:16:55 -0400

    Will Brooks Fix: wrong program for MasterPy.py initially committed.  This is the corrected version

--- Lab-1/MasterPy.py
+++ Lab-1/MasterPy.py
@@ -1,69 +1,27 @@
-def int2roman(num):
+import string
+import random
 
-	strOut = ""
+secret = [random.choice('ABCDEF') for item in range(4)]
+
+print("I've selected a 4-character secret cde fro the letters A,B,C,D,E,F.")
+print("I may have repeated some.")
+print("Now, try and guess what I chose.")
+
+yourGuess = []
+while list(yourGuess) != secret:
+	yourGuess = input("Enter a 4-letter guess: e.g. ABCD : ").upper()
+	if len(yourGuess)!=4:
+		continue
 	
-	if (num >= 5000) | (num <= 0):
-		raise ValueError
-
-	while num > 0:
-		if num >= 1000:
-			strOut = strOut + "M"
-			num = num - 1000
-			continue
-		if num >= 900:
-			strOut = strOut + "CM"
-			num = num - 900
-			continue
-		if num >= 500:
-			strOut = strOut + "D"
-			num = num - 500
-			continue
-		if num >= 400:
-			strOut = strOut + "CD"
-			num = num - 400
-			continue
-		if num >= 100:
-			strOut = strOut + "C"
-			num = num - 100
-			continue
-		if num >= 90:
-			strOut = strOut + "XC"
-			num = num - 90
-			continue
-		if num >= 50:
-			strOut = strOut + "L"
-			num = num - 50
-			continue
-		if num >= 40:
-			strOut = strOut + "XL"
-			num = num - 40
-			continue
-		if num >= 10:
-			strOut = strOut + "X"
-			num = num - 10
-			continue
-		if num >= 9:
-			strOut = strOut + "IX"
-			num = num - 9
-			continue
-		if num >= 5:
-			strOut = strOut + "V"
-			num = num - 5
-			continue
-		if num >= 4:
-			strOut = strOut + "IV"
-			num = num - 4
-			continue
-		if num >= 1:
-			strOut = strOut + "I"
-			num = num - 1
-			continue
-
+	print("You guessed " +  str(yourGuess))
 	
-	return strOut
+	comparingList = zip(secret, yourGuess)
 
+	correctList = [speg for speg, gpeg in comparingList if speg == gpeg]
 
-try:
-	print(int2roman(int(input("Enter a number between 1 ad 4999: "))))
-except ValueError:
-	print("Invalid number")
+	fewestLetters = [min(secret.count(j), yourGuess.count(j)) for j in 'ABCDEF']
+
+	print("Number of correct letters is ", len(correctList))
+	print("Number of unused letters is ", sum(fewestLetters) - len(correctList))
+
+print("YOU GOT THE ANSWER : ", secret)
